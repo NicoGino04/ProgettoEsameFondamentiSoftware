328eec093717eaa90e75d8cf818a02e54bf700a6
Nicola Longo <nickylongo04@gmail.com>
2025-12-27 19:09:23 +0100

    modifiche dei dati di base presenti nelle fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Goal;
@@ -10,19 +11,51 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $product = new Product();
-        $product->setName("Product One");
-        $product->setDescription("This is the first product");
-        $product->setSize(100);
+        $user = new User();
+        $user->setEmail("tu@tu.tu");
+        $user->setPassword("tututu");
+        $user->setRoles(["ROLE_ADMIN"]);
 
-        $manager->persist(object: $product);
+        $goal = new Goal();
+        $goal->setName("Bicchieri d'acqua");
+        $goal->setExpiration(new \DateTime("2026-06-30"));
+        $goal->setQuantity("3");
+        $goal->setGoalQuantity("100");
 
-        $product = new Product();
-        $product->setName("Product Two");
-        $product->setDescription("This is the second product");
-        $product->setSize(200);
+        $user->addGoal($goal);
 
-        $manager->persist(object: $product);
+        $goal = new Goal();
+        $goal->setName("Micronutrienti assunti");
+        $goal->setExpiration(new \DateTime("2026-06-30"));
+        $goal->setQuantity("6");
+        $goal->setGoalQuantity("600");
+
+        $user->addGoal($goal);
+
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail("io@io.io");
+        $user->setPassword("ioioio");
+        $user->setRoles(["ROLE_USER"]);
+
+        $goal = new Goal();
+        $goal->setName("Bicchieri d'acqua");
+        $goal->setExpiration(new \DateTime("2026-06-30"));
+        $goal->setQuantity("3");
+        $goal->setGoalQuantity("100");
+
+        $user->addGoal($goal);
+
+        $goal = new Goal();
+        $goal->setName("Macronutrienti assunti");
+        $goal->setExpiration(new \DateTime("2026-06-30"));
+        $goal->setQuantity("6");
+        $goal->setGoalQuantity("600");
+
+        $user->addGoal($goal);
+
+        $manager->persist($user);
 
         $manager->flush();
     }

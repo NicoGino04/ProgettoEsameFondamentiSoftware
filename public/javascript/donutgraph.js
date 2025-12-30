7f042b64c993aa7e1c36c13a38ef6ffc1900ef30
Nicola Longo <nickylongo04@gmail.com>
2025-12-30 23:45:36 +0100

    interazione funzionante modifica grafico macro

--- public/javascript/donutgraph.js
+++ public/javascript/donutgraph.js
@@ -1,38 +1,72 @@
+let chart
 document.addEventListener('DOMContentLoaded', function () {
 
-  const ctx = document
-    .getElementById('myDoughnutChart')
-    .getContext('2d');
-
-  const data = {
-    labels: ['Proteine', 'Carboidrati', 'Lipidi'],
-    datasets: [{
-      label: 'Esempio Doughnut',
-      data: [20, 50, 100], //valori di esempio del grafico a torta
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgba(43, 245, 87, 1)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      cutout: '70%',
-      plugins: {
-        title: {
-          display: true,
-          text: '' //testo sopra la legenda
+    fetch('/macronutrienti', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
         }
-      }
-    }
-  };
+    })
+        .then(response => {
+            return response.json().then(data => {
+                if (!response.ok) {
+                    throw new Error(data.error || 'Errore di caricamento');
+                }
+                return data;
+            });
+        })
+        .then(data => {
+            console.log('Macronutrienti:', data);
 
-  new Chart(ctx, config);
+            const ctx = document
+                .getElementById('myDoughnutChart')
+                .getContext('2d');
 
+            const chartData = {
+                labels: ['Carboidrati', 'Lipidi', 'Proteine'],
+                datasets: [{
+                    label: 'Macronutrienti',
+                    data: [
+                        data.carboidrati,
+                        data.grassi,
+                        data.proteine
+                    ],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgba(43, 245, 87, 1)',
+                        'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }]
+            };
+            const config = {
+                type: 'doughnut',
+                data: chartData,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    cutout: '70%',
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: '' //testo sopra la legenda
+                        }
+                    }
+                }
+            };
+
+            chart = new Chart(ctx, config);
+
+        })
+        .catch(error => {
+            console.error('Errore:', error);
+            Swal.fire({
+                icon: 'error',
+                title: error.message,
+                confirmButtonColor: '#d33'
+            });
+        })
+        .finally(() => {
+        })
 });
--- src/Repository/PastoRepository.php
+++ src/Repository/PastoRepository.php
@@ -16,20 +16,24 @@ class PastoRepository extends ServiceEntityRepository
         parent::__construct($registry, Pasto::class);
     }
 
-    //    /**
-    //     * @return Pasto[] Returns an array of Pasto objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+        /**
+         * @return Pasto[] Returns an array of Pasto objects
+         */
+        public function findByDateField($user, $data = new \DateTime()): array
+        {
+            return $this->createQueryBuilder('p')
+                ->andWhere('p.giorno = :giorno')
+                ->setParameter('giorno', $data->format('Y-m-d'))
+//                ->andWhere('p.pasto = :val')
+//                ->setParameter('val', "colazione")
+                ->andWhere('p.user= :user')
+                ->setParameter('user', $user->getId())
+                ->orderBy('p.id', 'ASC')
+                //->setMaxResults(10)
+                ->getQuery()
+                ->getResult()
+            ;
+        }
 
     //    public function findOneBySomeField($value): ?Pasto
     //    {
